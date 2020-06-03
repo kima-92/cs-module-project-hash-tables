18204459a5817b2c69dc3b81c66fd890e16b2678
Wilma <kima_92@fastmail.com>
2020-06-03 18:23:14 -0400

    Implemented expensive_seq.py using a python dict. Test Pass.

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,22 @@
-# Your code here
+
+xyz_cache= {}
 
 
 def expensive_seq(x, y, z):
-    # Your code here
+    seq = (x, y, z)
+
+    if seq in xyz_cache:
+        return xyz_cache[seq]
+    
+    else:
+        if x <= 0: 
+                xyz_cache[seq] = y + z
+                return xyz_cache[seq]
+        
+        if x > 0:
+            xyz_cache[seq] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+            return xyz_cache[seq]
+            
 
 
 
