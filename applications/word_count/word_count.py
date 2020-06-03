7999b781ad0f6bf1a303068cc532ca3bc9757f8e
Wilma <kima_92@fastmail.com>
2020-06-03 19:31:11 -0400

    Finished implementing word_count.py. Passes all Tests

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,35 @@
+
+ignored = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&', ' ']
+
 def word_count(s):
-    # Your code here
+
+    words = s.split()
+    words_dict = {}
+    bad_char = ""
+
+    for word in words:
+        new_word = ""
+        
+        for char in word:
+            if char in ignored:
+                bad_char += char
+            else:
+                new_word += char
+
+        word = new_word.lower()
+        
+        if word in words_dict:
+            words_dict[word] += 1
+
+        elif word == "" or word == " ":
+            break
+        else: 
+            words_dict[word] = 1
+    
+    if s == "":
+        return {}
+    else:
+        return words_dict
 
 
 
